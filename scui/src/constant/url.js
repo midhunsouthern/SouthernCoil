2898fcb7939139b0755040bef0c0699bb9feec29
Josseph-2432 <thejosephrajan@gmail.com>
2024-02-02 16:42:52 +0530

    const url

--- scui/src/constant/url.js
+++ scui/src/constant/url.js
@@ -1,7 +1,7 @@
-export const baseURL = "http://localhost:8080/scoil/index.php/";
-//export const baseURL = "http://southerncoil.local/index.php/";
-//export const baseURL = 'https://scuitest.southerncoil.com/backend/index.php/';
-//export const baseURL = "https://scui.southerncoil.com/backend/index.php/";
+// export const baseURL = "http://localhost:8080/scoil/index.php/";
+// export const baseURL_noIndex = "http://localhost:8080/scoil/";
+export const baseURL = "https://scui.southerncoil.com/backend/index.php/";
+export const baseURL_noIndex = "https://scui.southerncoil.com/backend/";
 export const loginURL = baseURL + "login/login_acc";
 export const getAccessTypeURL = baseURL + "main/access_type_list";
 export const getProfileDataURL = baseURL + "main/getProfileData";
@@ -62,3 +62,7 @@ export const updateSchedulerOrderDate =
 export const updateSchedulerCommitmentStatus =
 	baseURL + "main/updateSchedulerCommitmentStatus";
 export const dashboardGraphData = baseURL + "main/dashboardGraphData";
+
+/**image url */
+export const imageURL = baseURL_noIndex;
+export const getActiveOrderIds = baseURL + "main/getActiveOrders";
