8b866809573f506bba62e679336f2e2fa9da03b9
Gnana Sekar <developersekar1992@gmail.com>
2024-02-07 16:48:53 +0530

    Moved uuid to image_helper

--- application/helpers/image_helper.php
+++ application/helpers/image_helper.php
@@ -1,6 +1,18 @@
 <?php
 if (!defined('BASEPATH')) exit('No direct script access allowed');
+function generate_uuid() {
+    // Generate a v4 UUID
+    $data = openssl_random_pseudo_bytes(16);
+    assert(strlen($data) == 16);
 
+    // Set version to 0100
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+    // Set bits 6-7 to 10
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+    // Output the 36 character UUID.
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+}
 function convert_base64_to_webp($base64_image, $output_path, $webpName,$quality = 60)
 {
     $base64_image = preg_replace('/^data:image\/(jpeg|png|webp);base64,/', '', $base64_image);
@@ -68,4 +80,5 @@ function convert_file_to_webp($image_path, $output_path,$webpName, $quality = 80
         } else {
        return false;
     }
+ 
 }
